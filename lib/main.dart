1330ff9fead94b5539ada46b7b2e97e0df1ed5bc
Ariel Kerzhner <arielk2356@gmail.com>
2020-11-27 18:51:35 +0200

    init userSettingsScreen

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Increment' ,
         child: Icon(Icons.add),
       ),
     );
