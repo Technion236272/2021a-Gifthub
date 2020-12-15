b55376dc76500ad367c4f9ebb989097fd6d16184
Yahav <vyahav@campus.technion.ac.il>
2020-12-16 00:16:29 +0200

    Starting to work on checkout screen

---
+++ lib/checkoutScreen.dart
@@ -0,0 +1,18 @@
+import 'all_confetti_widget.dart';
+import 'my_flutter_app_icons.dart';
+import 'package:flutter/material.dart';
+//import 'package:firebase_core/firebase_core.dart';
+import 'user_repository.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+
+Widget checkoutScreenScaffold(context) => Scaffold(body:Center(child:
+Column(
+mainAxisAlignment: MainAxisAlignment.start,
+crossAxisAlignment: CrossAxisAlignment.center,
+children: <Widget>[
+
+
+]
+) ,));
